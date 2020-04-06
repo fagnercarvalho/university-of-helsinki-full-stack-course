2e4ee70398f0de8285d1a0c437fce376b4dca92d
Fagner Carvalho <fagnerncarvalho@gmail.com>
2020-04-06 02:28:19 -0300

    Add solution for unicafe, step 3 from part 1

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,6 +1,27 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Statistics = ({ good, neutral, bad }) => {
+  const formatNumber = (number) => number.toFixed(2)
+
+  const formatPercentage = (percentage) => formatNumber(percentage) + " %"
+
+  const averageFeedback = () => (good - bad) / (good + neutral + bad)
+
+  const positivePercentage = () => (good / (good + neutral + bad)) * 100
+
+  return (
+    <>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>average {formatNumber(averageFeedback())}</p>
+      <p>positive {formatPercentage(positivePercentage())}</p>
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
@@ -13,31 +34,18 @@ const App = () => {
 
   const setBadFeedback = () => setBad(bad + 1)
 
-  const formatNumber = (number) => number.toFixed(2)
-
-  const formatPercentage = (percentage) => formatNumber(percentage) + " %"
-
-  const averageFeedback = () => (good - bad) / (good + neutral + bad)
-
-  const positivePercentage = () => (good / (good + neutral + bad)) * 100
-
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1) }>good</button>
+      <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={setNeutralFeedback}>neutral</button>
       <button onClick={setBadFeedback}>bad</button>
 
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>average {formatNumber(averageFeedback())}</p>
-      <p>positive {formatPercentage(positivePercentage())}</p>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
 
-ReactDOM.render(<App />, 
+ReactDOM.render(<App />,
   document.getElementById('root')
 )
